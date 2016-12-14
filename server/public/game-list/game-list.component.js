9019ed02ce1fc4056102af7f5c99e14c2706f702
Rafael Oliveira <rafaeloliveira@Rafaels-MacBook-Air.local>
2016-12-14 14:16:26 +0000

    Design

--- server/public/game-list/game-list.component.js
+++ server/public/game-list/game-list.component.js
@@ -13,20 +13,27 @@ angular.
         self.games = response.data;
       });
     }]
-      /*this.games = [
-        {
-          name: 'Nexus S',
-          snippet: 'Fast just got faster with Nexus S.'
-        }, {
-          name: 'Motorola XOOM™ with Wi-Fi',
-          snippet: 'The Next, Next Generation tablet.'
-        }, {
-          name: 'MOTOROLA XOOM™',
-          snippet: 'The Next, Next Generation tablet.'
-        }
-      ];
 
-      this.orderProp = 'age';
-      */
-    
+  // create empty search model (object) to trigger $watch on update
+  /*$scope.search = {};
+
+  $scope.resetFilters = function () {
+    // needs to be a function or it won't trigger a $watch
+    $scope.search = {};
+  };
+
+  // pagination controls
+  $scope.currentPage = 1;
+  $scope.totalItems = $scope.items.length;
+  $scope.entryLimit = 8; // items per page
+  $scope.noOfPages = Math.ceil($scope.totalItems / $scope.entryLimit);
+
+  // $watch search to update pagination
+  $scope.$watch('search', function (newVal, oldVal) {
+    $scope.filtered = filterFilter($scope.items, newVal);
+    $scope.totalItems = $scope.filtered.length;
+    $scope.noOfPages = Math.ceil($scope.totalItems / $scope.entryLimit);
+    $scope.currentPage = 1;
+  }, true);
+    */
   });
